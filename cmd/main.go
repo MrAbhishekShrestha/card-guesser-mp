764d2e510192297547df63c884afb7b0b095fa9f
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2024-10-27 21:57:33 +0545

    read port from env variable

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/MrAbhishekShrestha/card-guesser-mp/cmd/api"
 )
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func main() {
-	server := api.NewAPIServer("localhost:3000")
+	// port := "localhost:3000"
+	port := getEnv("PORT", ":10000")
+	server := api.NewAPIServer(port)
 	if err := server.Run(); err != nil {
 		log.Fatalf("APIServer error: %v", err)
 	}
