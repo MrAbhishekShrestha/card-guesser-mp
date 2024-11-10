01bdf22b9dce0cd0dd7e4c1ade1c8ee9a773afa8
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2024-11-10 22:02:58 +0545

    bugfix: join room logic in the client

--- client/src/app/controllers/ws-store/ws.reducers.ts
+++ client/src/app/controllers/ws-store/ws.reducers.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from "@ngrx/store";
-import { initState } from "./ws.states";
+import { initState, WebSocketGameState } from "./ws.states";
 import { receivedCreateRoom, receivedEndGame, receivedJoinRoom, receivedRegisterPlayer, receivedStartGame, sendMove, sendSetName, webSocketConnected, webSocketDisconnected } from "./ws.actions";
 import { act } from "@ngrx/effects";
 
@@ -22,18 +22,22 @@ export const websocketReducer = createReducer(
       ...state.gameState, createRoomId: action.message.payload.roomId
     }
   })),
-  on(receivedJoinRoom, (state, action) => ({
-    ...state, gameState: {
-      ...state.gameState,
-      joinedRoom: {
-        id: action.message.payload.roomId,
-        timeoutInSeconds: action.message.payload.timeoutInSeconds,
-        players: [...action.message.payload.players],
-        status: state.gameState.joinedRoom?.status === null ? "EMPTY" : <string>state.gameState.joinedRoom?.status,
-        newGame: state.gameState.joinedRoom === null ? null : state.gameState.joinedRoom.newGame
+  on(receivedJoinRoom, (state, action) => {
+    const isNewRoom = action.message.payload.roomId !== state.gameState.joinedRoom?.id;
+    const newState: WebSocketGameState = {
+      ...state, gameState: {
+        ...state.gameState,
+        joinedRoom: {
+          id: action.message.payload.roomId,
+          timeoutInSeconds: action.message.payload.timeoutInSeconds,
+          players: [...action.message.payload.players],
+          status: isNewRoom ? "EMPTY" : <string>state.gameState.joinedRoom?.status,
+          newGame: isNewRoom ? null : state.gameState.joinedRoom?.newGame ?? null
+        }
       }
-    }
-  })),
+    };
+    return newState;
+  }),
   on(receivedStartGame, (state, action) => ({
     ...state, gameState: {
       ...state.gameState,
